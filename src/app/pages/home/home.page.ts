188b37ca9eb788101a065c84469d6e22315bd82a
nicolastartaglia <ntartaglia@gmail.com>
2021-05-24 23:50:15 +0200

    menu dynamique sur la page d'accueil

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  constructor(private animationCtrl: AnimationController) {
+
+  }
+
+  ngOnInit() {
+    const animation1: Animation = this.animationCtrl.create()
+    .addElement(document.querySelector('#it1'))
+    .duration(800)
+    .iterations(1)
+    .fromTo('transform', 'translateY(1000%)', 'translateY(30vh)')
+    .fromTo('opacity', '0.2', '1');
+
+    const animation2: Animation = this.animationCtrl.create()
+    .addElement(document.querySelector('#it2'))
+    .duration(850)
+    .iterations(1)
+    .fromTo('transform', 'translateY(1000%)', 'translateY(30vh)')
+    .fromTo('opacity', '0.2', '1');
+
+    const animation3: Animation = this.animationCtrl.create()
+    .addElement(document.querySelector('#it3'))
+    .duration(950)
+    .iterations(1)
+    .fromTo('transform', 'translateY(1000%)', 'translateY(30vh)')
+    .fromTo('opacity', '0.2', '1');
+
+
+    animation1.play();
+    animation2.play();
+    animation3.play();
+
+  }
+
+}
